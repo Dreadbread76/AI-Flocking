31aec86de8a0a13db1c880b67e252a210658cb60
Dreadbread76 <61036310+Dreadbread76@users.noreply.github.com>
2020-11-26 08:31:44 +1100

    Commit

---
+++ AI Flocking/Assets/Scripts/Flock/BlueAIStateMachine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueAIStateMachine : MonoBehaviour
+{
+
+    public BlueCompositeBehavior BlueComposite;
+    // Start is called before the first frame update
+   public enum Blue
+    {
+        Wander,
+        FleeNHide,
+    }
+    ;
+
+    public Blue blueStates;
+    IEnumerator BlueState()
+    {
+        while (blueStates == Blue.Wander)
+        {
+            BluePatrol();
+
+            yield return 0;
+        }
+        while (blueStates == Blue.FleeNHide)
+        {
+            BlueFleeNHide();
+
+            yield return 0;
+        }
+
+    }
+
+    public void BluePatrol()
+    {
+        Debug.Log("Wander Enter");
+        BlueComposite.Flocks[6].weight = 0;
+    }
+    public void BlueFleeNHide()
+    {
+        BlueComposite.Flocks[6].weight = 2;
+    }
+}
---
+++ AI Flocking/Assets/Scripts/Flock/RedAIStateMachine.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedAIStateMachine : MonoBehaviour
+{
+    public RedCompositeBehavior RedComposite;
+     GameObject enemyTarget;
+
+    // Start is called before the first frame update
+    public enum Red
+    {
+        Wander,
+        Pursue
+
+    }
+    ;
+    public Red redStates;
+    private void Start()
+    {
+        StartCoroutine(RedState());
+    }
+
+    private void Update()
+    {
+        enemyTarget = GameObject.FindGameObjectWithTag("Enemy");
+    }
+
+    IEnumerator RedState()
+    {
+        while (redStates == Red.Wander)
+        {
+            RedPatrol();
+
+            yield return 0;
+        }
+        while (redStates == Red.Pursue)
+        {
+            RedPursue();
+
+            yield return 0;
+        }
+
+    }
+
+    public void RedPatrol()
+    {
+        RedComposite.Flocks[6].weight = 0;
+    }
+    public void RedPursue()
+    {
+        RedComposite.Flocks[6].weight = 10;
+    }
+}
